885d5f1277dcebd4d0a96a935cc4198a74f95248
freefrog1986 <baihu1111@126.com>
2017-01-22 10:37:13 +0800

    add self.to_palces method

--- app/models/Place.rb
+++ app/models/Place.rb
@@ -1,6 +1,19 @@
 class Place
 include ActiveModel::Model
+attr_accessor :id, :formatted_address, :location, :address_components 
 
+def initialize(params)
+	@id = params[:_id].to_s
+	@formatted_address = params[:formatted_address]
+
+	@address_components = []
+    if !params[:address_components].nil?
+      address_components = params[:address_components]
+      address_components.each { |a| @address_components << AddressComponent.new(a) }
+    end
+	
+	@location = Point.new(params[:geometry][:geolocation])
+end
 
 def self.mongo_client
 	Mongoid::Clients.default
@@ -14,6 +27,18 @@ def self.load_all(f)
 	file=File.read(f)
     hash=JSON.parse(file)
     place=collection.insert_many(hash)
+end
+
+def self.find_by_short_name(s)
+	collection.find({"address_components.short_name":s})
+end
+
+def self.to_places(ms)
+	p = []
+    ms.each { |m| 
+      p << Place.new(m) 
+    }
+    return p
 end
 
 end
