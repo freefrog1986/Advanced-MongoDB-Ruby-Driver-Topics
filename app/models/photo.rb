2cae1d1f4cf661f4e2b849d8dc6ddfaf861c3502
freefrog1986 <baihu1111@126.com>
2017-01-27 00:03:00 +0800

    add initialize and persisted? instance method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,13 @@ def self.mongo_client
 	Mongoid::Clients.default
 end
 
+def initialize(params=nil)
+    @id = params[:_id].to_s if !params.nil? && !params[:_id].nil?
+    @location = Point.new(params[:metadata][:location]) if !params.nil? && !params[:metadata].nil?
+end
+
+def persisted?
+	!@id.nil?
+end
+
 end
