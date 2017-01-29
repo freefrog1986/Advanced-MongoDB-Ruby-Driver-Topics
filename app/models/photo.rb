7204cf7371c18f4b6708261f7897a8ca5e3931ab
freefrog1986 <baihu1111@126.com>
2017-01-29 11:13:27 +0800

    add save method into Photo class

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,4 +16,51 @@ def persisted?
 	!@id.nil?
 end
 
+# :contents is used to store the image 
+# f = File.open(’./db/image1.jpg’,’rb’)
+# photo.contents = f
+
+# to save instance you have to store the pic in database using GridFS, 
+# so you have to store @id and @location, while @content is the image itself
+# but how to get the gps info of the image? use EXIFR gem
+# Mongo::Grid::File.new() have two arguments, first one is the image(@contents.read),
+# the second one is description which store all the attributes.
+
+def save
+	# check whether the instance is already persisted
+    if !persisted?
+      # use the exifr gem to extract geolocation information from the jpeg image
+      gps = EXIFR::JPEG.new(@contents).gps
+      # Both EXIFR and GridFS will be reading the same file, 
+      # You must call rewind() on the file in between calls for the proper number 
+      # of bytes to be stored in GridFS.
+      @contents.rewind
+      # create a description array to store file property hashs
+      description={}
+      # store the content type of image/jpeg in the GridFS contentType file property
+      description[:content_type] = "image/jpeg"
+      # store the GeoJSON Point format of the image location
+      location=Point.new(:lng=>gps.longitude, :lat=>gps.latitude)
+      description[:metadata] = {
+        :location => location.to_hash
+      }
+      # store the data contents in GridFS
+      grid_file = Mongo::Grid::File.new(@contents.read, description)
+      # store the generated _id for the file in the :id property of the Photo model instance.
+      @id = self.class.mongo_client.database.fs.insert_one(grid_file).to_s
+      # sotre the location of Point class in the @location attribute
+      @location = Point.new(location.to_hash)
+    else
+    end
+end
+
+def method_name
+	
+end
+
+
+
+
+
+
 end
