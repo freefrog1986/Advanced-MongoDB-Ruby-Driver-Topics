a7ecd19c589183e0b9001713992cb1b3a63cc68b
freefrog1986 <baihu1111@126.com>
2017-01-31 10:40:38 +0800

    finish data tier population set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'pp'
+# 1. Clear GridFS of all files.
 Photo.all.each { |photo| photo.destroy }
+# 2. Clear the places collection of all documents.
 Place.all.each { |place| place.destroy }
+# 3. Make sure the 2dsphere index has been created for the nested geometry.geolocation property within the places collection.
 Place.create_indexes
+# 4. Populate the places collection using the db/places.json file from the provided bootstrap files in student-start.
 Place.load_all(File.open('./db/places.json'))
+# 5. Populate GridFS with the images also located in the db/ folder and supplied with the bootstrap files in student-start.
 Dir.glob("./db/image*.jpg") {|f| photo=Photo.new; photo.contents=File.open(f,'rb'); photo.save}
+# 6. For each photo in GridFS, locate the nearest place within one (1) mile
 Photo.all.each {|photo| place_id=photo.find_nearest_place_id 1*1609.34; photo.place=place_id; photo.save}
+# self-test
 pp Place.all.reject {|pl| pl.photos.empty?}.map {|pl| pl.formatted_address}.sort
